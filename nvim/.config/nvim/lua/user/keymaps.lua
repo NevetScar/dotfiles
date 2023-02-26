776df295bd29e6b4c997d3e947005f1f4ea68514
NevetScar <hstevencg7@gmail.com>
2023-02-26 14:05:01 -0600

    updated keymaps

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -14,30 +14,30 @@ keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
 
 -- leave visual, insert and visual block with Ctrl-s
-keymap("i", "", "<Esc>", opts)
-keymap("x", "", "<Esc>", opts)
-keymap("v", "", "<Esc>", opts)
+-- keymap("i", "<C-s>", "<Esc>", opts)
+-- keymap("x", "<C-s>", "<Esc>", opts)
+-- keymap("v", "<C-s>", "<Esc>", opts)
 
 -- create a line under and over to leave up and down
 
-keymap("n", "n", "o<C-[>j<S-o>", opts)
-keymap("n", "<leader>n", "o<Esc><S-o>", opts)
+-- keymap("n", "n", "o<C-[>j<S-o>", opts)
+-- keymap("n", "<leader>n", "o<Esc><S-o>", opts)
 
 -- copy line down
 
-keymap("n", "<leader>c", "yyp", opts)
+-- keymap("n", "<leader>c", "yyp", opts)
 
 -- visual mode move chunck up or down
 
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+-- keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+-- keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- normal mode move line up ro down
 
-keymap("n", "<A-j>", ":m .+1<CR>", opts)
-keymap("n", "<A-k>", ":m .-2<CR>", opts)
+-- keymap("n", "<A-j>", ":m .+1<CR>", opts)
+-- keymap("n", "<A-k>", ":m .-2<CR>", opts)
 
 -- visual block indent
 
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+-- keymap("v", "<", "<gv", opts)
+-- keymap("v", ">", ">gv", opts)
