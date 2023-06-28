3ff3bc4bdf11dacbc5ef3d9bb0a7a7b17ffe959e
NevetScar <hstevencg7@gmail.com>
2023-06-28 10:15:37 -0600

    added Lua for neovim

--- nvim-mine/.config/nvim/lua/user/lsp.lua
+++ nvim-mine/.config/nvim/lua/user/lsp.lua
@@ -28,8 +28,32 @@ vim.keymap.set('n', '<leader>rq', ht.repl.quit, def_opts)
 lspconfig.marksman.setup({})
 lspconfig.hls.setup({})
 lspconfig.bashls.setup({})
-lspconfig.lua_ls.setup({})
-
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          'vim',
+          'require'
+        },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 -- LSP Keybindings
 vim.api.nvim_create_autocmd('LspAttach', {
   desc = 'LSP actions',
