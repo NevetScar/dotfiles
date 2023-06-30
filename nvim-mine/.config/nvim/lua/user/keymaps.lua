0a72ee3a34e55806b1b16d2be8d506ab17fe8805
NevetScar <hstevencg7@gmail.com>
2023-06-30 07:51:38 -0600

    Created an align on token tool

--- nvim-mine/.config/nvim/lua/user/keymaps.lua
+++ nvim-mine/.config/nvim/lua/user/keymaps.lua
@@ -44,6 +44,38 @@ function M.selectNextWord()
   local keys = vim.api.nvim_replace_termcodes("g<C-h>",true,false,true)
   vim.api.nvim_feedkeys(keys,'m',false)
 end
-vim.api.nvim_set_keymap('n', '<leader>s', ':lua require("user/keymaps").selectNextWord()<CR>', { noremap = true, silent = false })
+vim.api.nvim_set_keymap('n', '<leader>s', ':lua require("user/keymaps").selectNextWord()<CR>', opts)
+
+-- Align on a token
+function M.alignT(t)
+  local start          = vim.api.nvim_buf_get_mark(0,'<')[1]
+  local endr           = vim.api.nvim_buf_get_mark(0,'>')[1]
+  local greatestcolumn = 0
+  local positionsofT   = {}
+  for line = start, endr do
+    vim.api.nvim_win_set_cursor(0,{line,0})
+    local posix = vim.fn.searchpos(t,"n")
+    if posix[2] > greatestcolumn then
+      greatestcolumn = posix[2]
+    end
+    table.insert(positionsofT,posix)
+  end
+  for _, markpos in pairs(positionsofT) do
+    vim.api.nvim_win_set_cursor(0,{markpos[1]-1,markpos[2]})
+    vim.api.nvim_buf_set_text(0,markpos[1]-1,markpos[2]-1,markpos[1]-1,markpos[2]-1,{string.rep(' ',greatestcolumn - markpos[2])})
+  end
+end
+
+-- Call alignT interactively
+function M.alignLinesOnTokenInteractive()
+    local token = vim.fn.input('Enter token: ')
+    if token then
+        M.alignT(token)
+    else
+        print('Invalid input')
+    end
+end
+
+vim.api.nvim_set_keymap('v', '<leader>a', ':lua require("user/keymaps").alignLinesOnTokenInteractive()<CR>', opts)
 
 return M
