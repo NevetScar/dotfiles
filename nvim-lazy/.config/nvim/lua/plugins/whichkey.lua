5f5478826aca517ae81a74d74c0534829601a1ce
NevetScar <hstevencg7@gmail.com>
2023-07-10 21:10:47 -0600

    fixed whick key

--- nvim-lazy/.config/nvim/lua/plugins/whichkey.lua
+++ nvim-lazy/.config/nvim/lua/plugins/whichkey.lua
@@ -1,9 +1,9 @@
 return
 {
   "folke/which-key.nvim",
-  config = function()
+  event = "VeryLazy",
+  init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 300
-    require("which-key").setup{}
-  end
+  end,
 }
