93dd1c6f922bb10de8e9c7fe1b8cc8d1ffdba0f7
NevetScar <hstevencg7@gmail.com>
2023-07-17 09:37:17 -0600

    Added lazygit

---
+++ nvim-lazy/.config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,10 @@
+return
+{
+    "kdheepak/lazygit.nvim",
+    cmd = {"LazyGit", "LazyGitCurrentFile"},
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+}
+
