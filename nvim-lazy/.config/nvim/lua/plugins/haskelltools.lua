fa54adcb5984898232a6c1de8a0e7ab8766b739f
NevetScar <hstevencg7@gmail.com>
2023-09-20 20:03:18 -0600

    updated haskell tools

--- nvim-lazy/.config/nvim/lua/plugins/haskelltools.lua
+++ nvim-lazy/.config/nvim/lua/plugins/haskelltools.lua
@@ -1,29 +1,10 @@
 return
 {
-  "mrcjkb/haskell-tools.nvim",
-  ft = {"haskell", "lhaskell"},
-  opts = {
-      hls = {
-        on_attach = function(client, bufnr)
-        require('telescope').load_extension('ht')
-        local ht = require('haskell-tools')
-        local opts   = function(desc_, buff)
-          return { noremap = true, silent = true, desc = desc_, buffer = buff}
-        end
-        local def_opts = function(desc_)
-          return { noremap = true, silent = true, desc = desc_}
-        end
-          vim.keymap.set('n', '<space>ca', vim.lsp.codelens.run, opts("Code lens", bufnr))
-          vim.keymap.set('n', '<space>hs', ht.hoogle.hoogle_signature, opts("Hoogle signature", bufnr))
-          vim.keymap.set('n', '<leader>rr', ht.repl.toggle, def_opts("Toggle repl"))
-          vim.keymap.set('n', '<leader>rf',
-                function ()
-                  ht.repl.toggle(vim.api.nvim_buf_get_name(0))
-                end
-                , def_opts("Focus repl"))
-          vim.keymap.set('n', '<leader>rq', ht.repl.quit, def_opts("Quit repl"))
-        end,
-      },
-    }
+  'mrcjkb/haskell-tools.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  version = '^2', -- Recommended
+  ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' },
 }
 
---
+++ nvim-lazy/.config/nvim/lua/user/after.lua
@@ -0,0 +1,18 @@
+-- Haskell tools
+require('telescope').load_extension('ht')
+local ht = require('haskell-tools')
+local opts   = function(desc_, buff)
+  return { noremap = true, silent = true, desc = desc_, buffer = buff}
+end
+local def_opts = function(desc_)
+  return { noremap = true, silent = true, desc = desc_}
+end
+  vim.keymap.set('n', '<space>ca', vim.lsp.codelens.run, opts("Code lens", bufnr))
+  vim.keymap.set('n', '<space>hs', ht.hoogle.hoogle_signature, opts("Hoogle signature", bufnr))
+  vim.keymap.set('n', '<leader>rr', ht.repl.toggle, def_opts("Toggle repl"))
+  vim.keymap.set('n', '<leader>rf',
+        function ()
+          ht.repl.toggle(vim.api.nvim_buf_get_name(0))
+        end
+        , def_opts("Focus repl"))
+  vim.keymap.set('n', '<leader>rq', ht.repl.quit, def_opts("Quit repl"))
