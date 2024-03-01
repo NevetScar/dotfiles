a6cc082f47b60fffbccebf650a7b6d6af9026859
NevetScar <hstevencg7@gmail.com>
2024-03-01 14:40:27 -0600

    added the sudo requirement

--- scripts/install-yay.sh
+++ scripts/install-yay.sh
@@ -4,7 +4,7 @@ if [[ -f '/usr/bin/yay' ]]; then
     echo "Congratulations, yay is installed!"
 else
     cd ~
-    pacman -S --needed git base-devel
+    sudo pacman -S --needed git base-devel
     git clone https://aur.archlinux.org/yay.git
     cd yay
     makepkg -si
