18391e36723f75a7bbc706894144ed51565dcaf1
NevetScar <hstevencg7@gmail.com>
2024-03-02 16:00:41 -0600

    fixed to edit etc

--- scripts/change-pacman-conf.sh
+++ scripts/change-pacman-conf.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 sed -i \
-    -e 's/#Color/Color/g' ./pacman.conf \
+    -e 's/#Color/Color/g' /etc/pacman.conf \
     -e 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' \
     -e 's/#\[multilib\]/\[multilib\]/g' \
     -e '/\[multilib\]/,/Include/{s/#//}' \
