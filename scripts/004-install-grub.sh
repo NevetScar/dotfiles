08de758d203a995895fab9963e6edfbad8e86bfd
NevetScar <hstevencg7@gmail.com>
2024-03-02 19:41:41 -0600

    fixed error in hyphenation

--- scripts/004-install-grub.sh
+++ scripts/004-install-grub.sh
@@ -9,6 +9,6 @@ done
 mkdir /boot/EFI
 mkdir /boot/grub
 mount $parti /boot/EFI
-grub-install --target=x86_64-efi –bootloader-id=grub_uefi –recheck
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 echo "please exit, and umount -a and reboot"
