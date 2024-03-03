aa82dafc9430c2186f2364f10be1a618d7d79a97
NevetScar <hstevencg7@gmail.com>
2024-03-02 18:38:58 -0600

    added bluez and stow packages

--- scripts/001-install-initial-packages.sh
+++ scripts/001-install-initial-packages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 
-pacman -S linux linux-headers neovim openssh networkmanager wpa_supplicant wireless_tools netctl lvm2 grub efibootmgr dosfstools os-prober mtools intel-ucode nmtui linux-firmware
+pacman -S linux linux-headers neovim openssh networkmanager wpa_supplicant wireless_tools netctl lvm2 grub efibootmgr dosfstools os-prober mtools intel-ucode nmtui linux-firmware stow bluez
