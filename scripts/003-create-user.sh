c189b07634ccc974abbf6049892026a3286ebcae
NevetScar <hstevencg7@gmail.com>
2024-03-02 19:19:42 -0600

    fixed interpolation

--- scripts/003-create-user.sh
+++ scripts/003-create-user.sh
@@ -3,12 +3,12 @@
 echo "Changing root password: "
 passwd
 user1=""
-while [-z $parti]
+while [-z "$parti"]
 do
   echo "Choose a username"
   read user1
 done
-useradd -m -g users -G wheel $user1
+useradd -m -g users -G wheel "$user1"
 passwd $user1 
 echo "uncomment %Wheel ALL=(ALL) ALL"
 EDITOR=nvim visudo
